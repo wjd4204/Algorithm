ee8e5e4f2b005658f7cfb854748a3771c84695e3
wjd4204 <110841041+wjd4204@users.noreply.github.com>
2024-07-26 00:05:10 +0900

    [level 1] Title: 체육복, Time: 0.57 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,39 +1,42 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int answer = 0;
-        
-        Arrays.sort(reserve);
+        int answer = n - lost.length;
+
         Arrays.sort(lost);
-        
-        // 도난 당하지 않은 학생 수
-        answer = n - lost.length;
-        
-        // 여벌 체육복을 가져왔지만 도난당한 학생 수
-        // 다른 학생에게 체육복을 빌려줄 수 없음
-        for (int i = 0; i < lost.length; i++) {
-			for (int j = 0; j < reserve.length; j++) {
-				if (lost[i] == reserve[j]) {
-					answer++;
-					lost[i] = -1;
-					reserve[j] = -1;
+        Arrays.sort(reserve);
+
+        /*
+        2가지 경우를 생각
+        1. 여유분이 있지만 도난당한 이들
+        2. 그냥 도난당한 이들
+         */
+
+        //1. 여유분이 있지만 도난당한 이들
+        for(int i=0;i< lost.length;i++){
+            for(int j=0;j< reserve.length;j++){
+                if(lost[i] == reserve[j]){
+                    lost[i] = -1;
+                    reserve[j] = -1;
+                    answer += 1;
                     break;
-				}
-			}
-		}
-        
-        // 도난당했지만 체육복을 빌릴 수 있는 학생 수
-        for (int i = 0; i < lost.length; i++) {
-			for (int j = 0; j < reserve.length; j++) {
-				if (lost[i] - 1 == reserve[j] || lost[i] + 1 == reserve[j]) {
-					answer++;
-					reserve[j] = -1;
-					break;
-				}
-			}
-		}
+                }
+            }
+        }
+
+        //2. 그냥 도난당한 애들
+        for(int i=0;i< lost.length;i++){
+            for(int j=0;j< reserve.length;j++){
+                if(lost[i] - 1 == reserve[j] || lost[i] + 1 == reserve[j]){
+                    reserve[j] = -1;
+                    answer += 1;
+                    break;
+                }
+            }
+        }
         
         return answer;
-    }
+    }    
+
 }
