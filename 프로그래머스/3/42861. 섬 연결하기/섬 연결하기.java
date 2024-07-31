7bd49e7f32393a18dbcc783b0cde643db7d541c8
wjd4204 <110841041+wjd4204@users.noreply.github.com>
2024-07-31 18:30:51 +0900

    [level 3] Title: 섬 연결하기, Time: 17.71 ms, Memory: 79.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+class Solution {
+    
+    static int[] parent;
+    static void union(int a, int b){
+        // a와 b의 부모 노드 찾기
+        a = find(a);
+        b = find(b);
+
+        // a의 부모노드가 더 b의 부모노드보다 크면 parent[a] = b 아니면 반대
+        if(a>b)
+            parent[a] = b;
+        else parent[b] = a;
+    }
+    static int find(int x){
+        if(parent[x] == x)
+            return x;
+        else return find(parent[x]);
+    }
+    
+    public int solution(int n, int[][] costs) {
+        int answer = 0;
+        Arrays.sort(costs, (o1, o2) -> Integer.compare(o1[2], o2[2]));
+
+        for(int i=0;i<costs.length;i++){
+            System.out.println(costs[i][0] + " " + costs[i][1] + " " + costs[i][2]);
+        }
+
+        parent = new int[n];
+        for(int i=0;i<n;i++){
+            parent[i] = i;
+        }
+        // 크루스칼 알고리즘 사용하기
+        for(int i=0;i<costs.length;i++){
+
+            // i번째 노드들의 부모노드가 같지 않으면 다른 집합임을 의미.
+            if(find(costs[i][0]) != find(costs[i][1])){
+                union(find(costs[i][0]), find(costs[i][1]));
+                answer += costs[i][2];
+                continue;
+            }
+        }
+        
+        return answer;
+    }
+}
