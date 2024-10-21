f6a0db6eef223cd56592e1df51a06c2dd3f670d2
wjd4204 <110841041+wjd4204@users.noreply.github.com>
2024-10-21 21:14:58 +0900

    [level 1] Title: 최소직사각형, Time: 1.52 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -2,30 +2,12 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] sizes) {
-        int answer = 0;
-        
-        
-        // 가로든 세로든 대소비교를 통해 긴쪽과 짧은쪽을 분리
-        for(int i=0;i<sizes.length;i++){
-            if(sizes[i][0] > sizes[i][1]){
-                int tmp = sizes[i][0];
-                sizes[i][0] = sizes[i][1];
-                sizes[i][1] = tmp;
-            }
+        int length = 0, height = 0;
+        for (int[] card : sizes) {
+            length = Math.max(length, Math.max(card[0], card[1]));
+            height = Math.max(height, Math.min(card[0], card[1]));
         }
-
-        // 가로와 세로를 저장하는 List를 각각 만들고 sort를 이용해 내림차순으로 만들기
-        List<Integer> width = new ArrayList<>();
-        List<Integer> length = new ArrayList<>();
-        for(int[] i : sizes){
-            width.add(i[0]);
-            length.add(i[1]);
-        }
-        Collections.sort(width, Comparator.reverseOrder());
-        Collections.sort(length, Comparator.reverseOrder());
-        
-        // 첫 번째 값들이 가장 크므로 두 List의 첫 번째 값의 곱을 저장하여 반환
-        answer = width.get(0) * length.get(0);
+        int answer = length * height;
         return answer;
     }
 }
