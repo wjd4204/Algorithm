0ee3a5d9deeafc540a024868f4c9ca772c2944fc
wjd4204 <brian876373@gmail.com>
2025-04-24 22:35:27 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.13 ms, Memory: 57.1 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,47 +1,51 @@
 import java.util.*;
 
 class Point{
-    public int x, y;
-    Point(int y, int x){
+    int x;
+    int y;
+    public Point(int y, int x){
         this.x = x;
         this.y = y;
     }
 }
 
 class Solution {
-    static int[] x = {-1, 0, 1, 0};
-    static int[] y = {0, 1, 0, -1};
-    static int[][] visited;
-        
-    public int solution(int[][] maps) {
-        int answer = 9999;
-        visited = new int[maps.length][maps[0].length];
-        
-        BFS(0,0, maps);
-        if(visited[maps.length-1][maps[0].length-1] == 0)
-            return -1;
-        else
-            return visited[maps.length-1][maps[0].length-1];
-    }
     
-    public void BFS(int currentX, int currentY, int[][] maps){
+    int[] xPosition = {-1, 0, 1, 0};
+    int[] yPosition = {0, 1, 0, -1};
+    int[][] visited;
+    
+    public void bfs(int[][] maps){
+        
         Queue<Point> q = new LinkedList<>();
-        q.offer(new Point(currentX, currentY));
-        visited[currentY][currentX] = 1;
+        q.offer(new Point(0,0));
+        visited[0][0] = 1;
         
         while(!q.isEmpty()){
-            Point point = q.poll();
+            Point p = q.poll();
             for(int i=0;i<4;i++){
-                int newX = point.x + x[i];
-                int newY = point.y + y[i];
-                if(newX >= 0 && newX < visited[0].length && newY >= 0 && newY < visited.length && maps[newY][newX] == 1){
-                    maps[newY][newX] = 0;
-                    q.offer(new Point(newY, newX));
-                    visited[newY][newX] = visited[point.y][point.x] + 1;
+                int newX = p.x + xPosition[i];
+                int newY = p.y + yPosition[i];
+                if(newX >= 0 && newX < visited[0].length && newY >= 0 && newY < visited.length){
+                    if(maps[newY][newX] == 1){
+                        maps[newY][newX] = 0;
+                        q.offer(new Point(newY, newX));
+                        visited[newY][newX] = visited[p.y][p.x] + 1;
+                    }
                 }
             }
-            
         }
     }
-
+    
+    public int solution(int[][] maps) {
+        int answer = 0;
+        visited = new int[maps.length][maps[0].length];
+        
+        bfs(maps);
+        if(visited[maps.length-1][maps[0].length - 1] == 0)
+            answer = -1;
+        else answer = visited[maps.length-1][maps[0].length - 1];
+        
+        return answer;
+    }
 }
