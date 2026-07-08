2ed8fc1bd5a241d0bde95bc439a3b4c26a2734eb
wjd4204 <brian876373@gmail.com>
2026-07-08 11:18:17 +0900

    [level 1] Title: 삼총사, Time: 0.26 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -1,33 +1,30 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int[] number) {
-        int answer = 0;
-        
-        List<Integer> list = new ArrayList<>();
-        for(int i=0;i<number.length;i++){
-            list.add(number[i]);
-        }
-        Collections.sort(list);
-        
-        for(int i=0;i<list.size()-2;i++){
-            for(int j=i+1;j<list.size()-1;j++){
-                for(int k=j+1;k<list.size();k++){
-                    if(list.get(i) + list.get(j) + list.get(k) == 0)
-                        answer+=1;
-                }
+    
+    int answer = 0;
+    
+    public void combination(int[] list, int start, int depth, 
+                            int n, int r, int sum){
+        if(depth == r){
+            if(sum == 0){
+                answer++;
+                return;
             }
         }
         
+        for(int i=start;i<n;i++){
+            combination(list, i+1, depth+1, n, r, sum+list[i]);
+        }
+    }
+    
+    public int solution(int[] number) {
+        
+        combination(number, 0, 0, number.length, 3, 0);
         
         return answer;
     }
 }
 /*
--5, -2, 0, 2, 3
--5, 2, 3
-2, 0, -2
-어떻게 찾냐?
-3개 더하면 되지.
-일단 2개로 먼저 더하고, 더한 값의 반대 수의 index가 j보다 크면 answer에 추가
+순서를 고려하지 않은 조합 문제
 */
