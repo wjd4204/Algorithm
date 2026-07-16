563b789d7f02fcee6e891d662c134853d6b9996f
wjd4204 <brian876373@gmail.com>
2026-07-16 15:19:43 +0900

    [level 2] Title: 기능개발, Time: 0.37 ms, Memory: 85.1 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,43 +2,38 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        List<Integer> days = new ArrayList<>();
+        List<Integer> al = new ArrayList<>();
         
-        Stack<Integer> stack = new Stack<>();
-        int first = 0;
-        
+        Stack<Integer> st = new Stack<>();
+        int n = 100;
         for(int i=0;i<progresses.length;i++){
-            int day = (100 - progresses[i]); // 남은 일 수 구하기
-            if(day % speeds[i] != 0){
-                day = day / speeds[i] + 1;
-            }
-            else {
-                day = day/ speeds[i];
+            int lp = (100 - progresses[i]); // 남은 퍼센티지
+            int completedNum = lp / speeds[i]; 
+            if(lp % speeds[i] != 0){
+                completedNum += 1;
+            } // 남은 일 수 구하기
+            
+            if(st.empty() || i == 0){
+                st.push(completedNum);
+                n = completedNum;
+                continue;
             }
             
-            if(stack.isEmpty()){ // 비어있으면 첫번째 업무 push
-                stack.push(day);
-                first = stack.peek();
-            }
-            else{
-                //System.out.println(first + " " + day);
-                if(first >= day){ // 안에 들어있는 값이 현재 값보다 클 때, push하기
-                    stack.push(day);
-                }
-                else{ // 안에 들어있는 값이 현재 값보다 작을 때, 그동안의 업무들을 days에 업데이트하기
-                    days.add(stack.size());
-                    stack.clear();
-                    stack.push(day);
-                    first = day;
-                }
+            if(n >= completedNum) // 현재 작업이 앞 작업보다 같거나 빠를 경우, stack에 저장
+                st.push(completedNum);
+            else { // 현재 작업이 앞 작업보다 느리면, 지금까지 있던 내용 저장
+                al.add(st.size());
+                st.clear();
+                st.push(completedNum);
+                n = completedNum;
             }
         }
+        al.add(st.size());
         
-        if(stack.size() != 0)
-            days.add(stack.size());
-        System.out.println(stack);
-        
-        int[] answer = days.stream().mapToInt(Integer::intValue).toArray();
+        int[] answer = new int[al.size()];
+        for(int i=0;i<al.size();i++){
+            answer[i] = al.get(i);
+        }
         
         return answer;
     }
