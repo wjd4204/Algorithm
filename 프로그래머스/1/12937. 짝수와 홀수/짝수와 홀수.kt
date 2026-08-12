650a197c7ad47614b02db946f058e6d2bd911ec2
wjd4204 <brian876373@gmail.com>
2026-08-12 23:02:19 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.01 ms, Memory: 59.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.kt
@@ -0,0 +1,16 @@
+class Solution {
+    fun solution(num: Int): String {
+        var answer = ""
+        
+        answer = when {
+            num % 2 == 0 -> {
+                "Even"
+            }
+            else -> {
+                "Odd"
+            }
+        }
+        
+        return answer
+    }
+}
